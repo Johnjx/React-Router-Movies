313e3d2d89a15a4c00ade20a2c3f69c11ecce11a
John Ruddock <john.jxr@pm.me>
2022-04-25 19:08:50 -0400

    DRY code with flexible MovieCard comp

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {
   useParams
 } from 'react-router-dom'
+import MovieCard from './MovieCard';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
@@ -32,27 +33,13 @@ export default function Movie(props) {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
-
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+        <MovieCard movie = {movie} id={id}/>
       <div className="save-button">Save</div>
     </div>
   );
 }
+
+//movie details
+// use moviecard
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function MovieCard (props) {
-  return;
+
+  const { movie, id } = props;
+
+  return (
+    <div className="movie-card">
+      <h2>{movie.title}</h2>
+      <div className="movie-director">
+        Director: <em>{movie.director}</em>
+      </div>
+      <div className="movie-metascore">
+        Metascore: <strong>{movie.metascore}</strong>
+      </div>
+      {id && <h3>Actors</h3>}
+      {id && movie.stars.map(star => (
+        <div key={star} className="movie-star">
+          {star}
+        </div>  
+      ))}
+     </div> 
+  )
 }
